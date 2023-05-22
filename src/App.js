4a0d87a4e6342ec6378a75482a63eeab67f9fd79
Peter Shahin <petershahin00@gmail.com>
2023-05-22 18:35:04 +0300

    get data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+
+
+
+
+
+
+
+  const [data, setData] = useState([{}]);
+
+  const getData = async () => {
+    await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=44b96171e1c847c393bfbe612db4133e')
+
+      .then((response) => {
+        setData(response.data.articles)
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
-       
+
       </header>
     </div>
   );
