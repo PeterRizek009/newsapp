41655c194cec19fb836ee87997eafce17365a24f
Peter Shahin <petershahin00@gmail.com>
2023-06-28 16:13:19 +0300

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -19,9 +19,12 @@ const images = [
     {
         id: 4,
         url: "images/5.png"
-    }
-
-
+    },
+    {
+        id: 5,
+        url: "images/3.png"
+    },
+   
 
 ]
 
@@ -44,19 +47,19 @@ const icons = [
 ]
 const Footer = () => {
     return (
-        <footer className='grid grid-rows-2 w-full bottom-0 mt-6'>
-            <div className='relative row-span-1 flex flex-row justify-between flex-wrap md:flex-nowrap  w-full mx-auto bg-[#393939]'>
+        <footer className='grid grid-rows-2 md:w-full bottom-0 mt-6'>
+            <div className='relative row-span-1 flex flex-row justify-start flex-wrap md:w-full min-w-[380px] bg-[#393939]'>
                 {images.map((image) => (
-                    <img src={image.url} className='block w-[197px] h-[160px] md:w-full md:h-[340px] bg-center' alt='footerimg' />
+                    <img src={image.url} className='block md:w-[420px] w-[120px] h-[80px] md:h-[340px] bg-center' alt='footerimg' />
                 ))}
 
             </div>
 
-            <div className='relative row-span-1 flex justify-start items-center w-full bg-[#393939]'>
-                <div className='relative w-[25%] h-[300px] flex flex-column justify-center items-center flex-wrap my-6 mx-10'>
+            <div className='relative row-span-1 flex md:flex-row flex-column flex-wrap justify-start items-center md:w-full min-w-[400px] bg-[#393939]'>
+                <div className='relative md:w-[25%] w-[60%] h-[300px] flex flex-column justify-center items-center flex-wrap my-6 mx-10'>
                     <img src={logo} alt='logo' />
-                    <p className='text-white w-[50%] py-6 mx-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, dolores. </p>
-                    <div className='icons w-[50%] md:flex justify-between gap-1 py-6'>
+                    <p className='text-white md:w-[50%] w-full py-6 mx-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, dolores. </p>
+                    <div className='icons md:w-[50%] w-full flex md:justify-between justify-around gap-1 py-6'>
                         {icons.map(({ item, index }) => (
                             <div className='block icon' key={index}>
                                 {item}
@@ -65,25 +68,9 @@ const Footer = () => {
                         )}
                     </div>
                 </div>
-                <div className='relative  w-[25%]  h-[300px] flex flex-column justify-normal items-normal flex-wrap my-6'>
-                <h1 className='w-[250px] border-gradient-red-white'>Photo Gallery</h1>
-                <div className='w-[400px] flex justify-start items-center flex-wrap my-10'>
-                {images.map((image) => (
-                    <img src={image.url} className='block w-[150px] h-[120px] m-1' alt='footerimg' />
-                ))}
-
-            </div>
-
-                </div>
-                <div className='relative  w-[25%]  flex flex-column justify-normal items-normal flex-wrap my-6'>
-                <h1 className='w-[250px] border-gradient-red-white'>About us</h1>
-
-                </div>
-                <div className='relative  w-[25%]  flex flex-column justify-normal items-normal flex-wrap my-6'>
-                    <img src={logo} alt='logo' />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, dolores. </p>
-
-                </div>
+            
+             
+             
 
             </div>
         </footer>
